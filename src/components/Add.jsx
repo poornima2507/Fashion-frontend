eb624876a604cc4f59368e83cdefd91e66f4033d
poornima2507 <poornimaofficial25@gmail.com>
2024-06-13 03:24:35 +0530

    created

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,38 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NavBar from './NavBar'
+import axios from 'axios'
 
 const Add = () => {
+    const [data,setData] = useState(
+        {
+           "id":"",
+           "name":"",
+           "price":"",
+           "image":""
+        }
+    )
+    const inputHandler = (event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+    axios.post("http://localhost:8088/add",data).then(
+        (response)=>{
+            console.log(data)
+            if (response.data.status=="success"){
+                alert("Successfully Added")
+            }
+            else{
+                alert("Error")
+            }
+        }
+    ).catch().finally()
+}
   return (
     <div>
+        <NavBar/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-x-12 col-xxl-12">
                             <label htmlFor="" className="form-label">ID</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='id' value={data.id} onChange={inputHandler} />
                         </div>
                     </div>
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-x-12 col-xxl-12">
                             <label htmlFor="" className="form-label">Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler} />
                         </div>
                     </div>
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-x-12 col-xxl-12">
                             <label htmlFor="" className="form-label">Price</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='price' value={data.price} onChange={inputHandler} />
                         </div>
                     </div>
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-x-12 col-xxl-12">
                             <label htmlFor="" className="form-label">Image</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='image' value={data.image} onChange={inputHandler} />
                         </div>
                     </div>
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-x-12 col-xxl-12">
-                           <button className="btn btn-success">Submit</button>
+                           <button className="btn btn-success" onClick={readValue}>Submit</button>
                         </div>
                     </div>
                 </div>
